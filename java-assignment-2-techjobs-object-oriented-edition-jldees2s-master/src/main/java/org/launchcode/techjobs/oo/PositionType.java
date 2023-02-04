49af034715011178714cf03826fffbf0b89a76cb
“jldes2s” <jordanldees@gmail.com>
2023-02-04 10:17:53 -0600

    PositionType Class Updates

---
+++ java-assignment-2-techjobs-object-oriented-edition-jldees2s-master/src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -0,0 +1,59 @@
+package org.launchcode.techjobs.oo;
+
+public class PositionType {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public PositionType() {
+        id = nextId;
+        nextId++;
+    }
+
+    public PositionType(String value) {
+        this();
+        this.value = value;
+    }
+
+    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    public String toString() {
+        return value;
+    }
+
+    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    //  their id fields match.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PositionType that = (PositionType) o;
+
+        if (id != that.id) return false;
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+
+    // Getters and Setters:
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
